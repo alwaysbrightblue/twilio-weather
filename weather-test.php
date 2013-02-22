76a327f65f5cdbcd16512f607910f795bcd715dd
alwaysbrightblue <gaberator@mac.com>
2013-02-21 21:15:05 -0500

    Finxed API for WeatherUnderground request
    Fixed the call request from the tree.  Now works!

--- weather-test.php
+++ weather-test.php
@@ -3,8 +3,13 @@
 //weatherunderground api query
 	//try to make the area dynamically change the location.
 	// Reference URL: http://www.wunderground.com/weather/api/d/docs?d=data/conditions
-	$weather_url = file_get_contents("http://api.wunderground.com/api/55f2cd0398a1022a/conditions/q/PA/Kutztown.json");
+	$weather_url = file_get_contents("http://api.wunderground.com/api/55f2cd0398a1022a/conditions/q/19530.json");
 	$weather_output = json_decode($weather_url);
+	
+	//and then we have to make each element a variable so Twilio can read it
+	$w_conditions = $weather_output->current_observation->weather;
+	$w_temp_f = $weather_output->current_observation->temp_f;
+	$w_wind_string = $weather_output->current_observation->wind_string;
 	
 	/* TESTING TO SEE THE DATA */
 				echo "<pre>";
@@ -18,26 +23,9 @@
 <h2>
 <?php 
 
-echo $weather_output->weather . "<br>";
-echo $weather_output->temp_f . "<br>";
-echo $weather_output->wind_string . "<br>";
+echo $w_conditions . "<br>";
+echo $w_temp_f. "<br>";
+echo $w_wind_string . "<br>";
 
 ?>
 </h2>
-
-<?php 
-/* ASSIGN MY FACEBOOK  */
-				$graph_url = file_get_contents('https://graph.facebook.com/jgabriel.lloyd');
-				$graph_output = json_decode($graph_url);
-
-
-			/* TESTING TO SEE THE DATA */
-				echo "<pre>";
-				print_r($graph_output);
-			    echo "</pre>";
-				
-				// PRINT OUT THE RESULTS */
-				echo $graph_output->name . "<br>";
-				echo $graph_output->gender . "<br />";
-				
-				?>
